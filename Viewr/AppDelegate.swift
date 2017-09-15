b4ff59c84b155e711e2d72d31d66a14fa2e7296d
Rhianne Price <price.rhianne@gmail.com>
2017-09-15 13:44:34 +1200

    Initial Commit

---
+++ Viewr/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Viewr
+//
+//  Created by Rhianne Price on 15/09/17.
+//  Copyright © 2017 Rhianne Price. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
