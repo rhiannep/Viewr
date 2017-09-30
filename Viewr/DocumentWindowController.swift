87c969e4b681d0f89cd2d91b16f25dbc202ec1b1
Rhianne Price <price.rhianne@gmail.com>
2017-09-30 14:35:37 +1300

    add some comments before major refactor

--- Viewr/DocumentWindowController.swift
+++ Viewr/DocumentWindowController.swift
@@ -9,28 +9,54 @@
 import Cocoa
 import Quartz
 
+// Controller for the main window of the viewer. Coordinates the side bar with the two outline views,
+// the pdf view, and the lecture tool bar.
 class DocumentWindowController: NSWindowController, NSWindowDelegate {
-    
-
     @IBOutlet weak var bookmarkOutline: BookmarkOutline!
-    @IBOutlet weak var pdfView: PDFView!
-    @IBOutlet weak var toolBarTitle: NSTextField!
-    @IBOutlet weak var toolBar: NSView!
-    @IBOutlet weak var lectureOutline: LectureSetOutline!
-    @IBOutlet weak var outlineView: NSOutlineView!
     @IBOutlet weak var bookmarkOutlineView: NSOutlineView!
     
+    @IBOutlet weak var lectureOutline: LectureSetModel!
+    @IBOutlet weak var lectureOutlineView: NSOutlineView!
+    
+    @IBOutlet weak var pdfView: PDFView!
+    
+    @IBOutlet weak var toolBarTitle: NSTextField!
+    @IBOutlet weak var toolBar: NSView!
+
     var openDocuments = 0
     var openDocumentNames = [URL]()
     var bookmarkCount = 0
     
+    // The current document in the view
+    var selectedDocument : PDFDocument? = nil {
+        didSet {
+            pdfView.document = selectedDocument
+            let name = selectedDocument?.documentURL?.lastPathComponent
+            window?.title = "\(name!) (\(openDocuments) open)"
+            toolBarTitle.stringValue = name!
+            toolBar.isHidden = false
+        }
+    }
     
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        lectureOutline?.set(window: self)
+        bookmarkOutline?.set(owner: self)
+        self.lectureOutlineView.reloadData()
+        
+        // Observer to change the lecture outline view when the pdf is scrolled.
+        NotificationCenter.default.addObserver(self, selector: #selector(DocumentWindowController.pdfViewScolled), name: .PDFViewPageChanged, object: nil)
+    }
+    
+    // convenience initialiser loads the correct view from it's nib file
     convenience init() {
         self.init(windowNibName: NSNib.Name(rawValue: "DocumentWindow"))
     }
     
-
-
+    // Function to handle the opening of documents
+    // triggered by the "+" button at the top of the lecture list
+    // The most recently opened document is selected in the outline view.
+    // Can open mutiple documents at once.
     @IBAction func openPDF(_ sender: Any) {
         let openPanel = NSOpenPanel()
         openPanel.allowsMultipleSelection = true
@@ -49,72 +75,64 @@ class DocumentWindowController: NSWindowController, NSWindowDelegate {
                             self.selectedDocument = self.lectureOutline.get(byURL: url)
                             document = self.selectedDocument!
                         }
-                        self.outlineView.reloadData()
+                        self.lectureOutlineView.reloadData()
                         self.selectedDocument = document
-                        self.outlineView.selectRowIndexes(IndexSet(integer: self.outlineView.row(forItem: document)), byExtendingSelection: false)
+                        self.lectureOutlineView.selectRowIndexes(IndexSet(integer: self.lectureOutlineView.row(forItem: document)), byExtendingSelection: false)
                     }
                 }
             }
         })
     }
-    var selectedDocument : PDFDocument? = nil {
-        didSet {
-            pdfView.document = selectedDocument
-            let name = selectedDocument?.documentURL?.lastPathComponent
-            window?.title = "\(name!) (\(openDocuments) open)"
-            toolBarTitle.stringValue = name!
-            toolBar.isHidden = false
-        }
-    }
-
-    override func windowDidLoad() {
-        super.windowDidLoad()
-        lectureOutline?.set(window: self)
-        bookmarkOutline?.set(owner: self)
-        self.outlineView.reloadData()
-        NotificationCenter.default.addObserver(self, selector: #selector(DocumentWindowController.pdfViewScolled), name: .PDFViewPageChanged, object: nil)
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-    }
     
+    // change the selcted document in the outline view when the page changes in the pdfview
     @objc func pdfViewScolled(_ notification: NSNotification) {
         let page = pdfView?.currentPage
-        outlineView.selectRowIndexes(IndexSet(integer: outlineView.row(forItem: page)), byExtendingSelection: false)
+        lectureOutlineView.selectRowIndexes(IndexSet(integer: lectureOutlineView.row(forItem: page)), byExtendingSelection: false)
     }
     
+    // called when the lecture selction changes in the lecture outline view.
+    // scrolls to the appropriate page on the pdfview
     func lectureSelectionDidChange(_ item: Any) {
         if let document = item as? PDFDocument {
             selectedDocument = document
             pdfView.go(to: document.page(at: 0)!)
             toolBarTitle.stringValue = (document.documentURL?.lastPathComponent)!
+            lectureOutlineView.selectRowIndexes(IndexSet(integer: lectureOutlineView.row(forItem: document)), byExtendingSelection: false)
         } else if let page = item as? PDFPage {
             selectedDocument = page.document
             pdfView.go(to: page)
             toolBarTitle.stringValue = "\((page.document?.documentURL?.lastPathComponent)!) page \(page.label!)"
         } else if let bookmark = item as? Bookmark {
-            outlineView.selectRowIndexes(IndexSet(integer: outlineView.row(forItem: bookmark.page)), byExtendingSelection: false)
+            lectureOutlineView.selectRowIndexes(IndexSet(integer: lectureOutlineView.row(forItem: bookmark.page)), byExtendingSelection: false)
         }
-        outlineView.scrollRowToVisible(outlineView.selectedRow)
+        lectureOutlineView.scrollRowToVisible(lectureOutlineView.selectedRow)
     }
     
+    // Changes the selection on the lecture outline view to the previous item when the "previous" control is hit.
+    // The pdfview then changes when the lecture outline delegate is notified.
     @IBAction func goToPreviousOutlineItem(_ sender: Any) {
-        let previous = outlineView.item(atRow: outlineView.selectedRow - 1)
+        let previous = lectureOutlineView.item(atRow: lectureOutlineView.selectedRow - 1)
         if let lecture = previous as? PDFDocument {
-            outlineView.expandItem(lecture)
+            lectureOutlineView.expandItem(lecture)
         }
-        outlineView.selectRowIndexes(IndexSet(integer: outlineView.selectedRow - 1), byExtendingSelection: false)
+        lectureOutlineView.selectRowIndexes(IndexSet(integer: lectureOutlineView.selectedRow - 1), byExtendingSelection: false)
     }
     
+    // Changes the selection on the lecture outline view to the next item when the "next" control is hit.
+    // The pdfview then changes when the lecture outline delegate is notified.
     @IBAction func goToNextOutlineItem(_ sender: Any) {
-        let this = outlineView.item(atRow: outlineView.selectedRow)
+        let this = lectureOutlineView.item(atRow: lectureOutlineView.selectedRow)
         if let lecture = this as? PDFDocument {
-            outlineView.expandItem(lecture)
+            lectureOutlineView.expandItem(lecture)
         }
-        outlineView.selectRowIndexes(IndexSet(integer: outlineView.selectedRow + 1), byExtendingSelection: false)
+        lectureOutlineView.selectRowIndexes(IndexSet(integer: lectureOutlineView.selectedRow + 1), byExtendingSelection: false)
     }
     
+    // Function to add a new bookmark for the current page in the pdf view to the bookmark model.
+    // Handles changes to the bookmark outline view.
     @IBAction func newBookmark(_ sender: Any) {
         if let page = pdfView.currentPage {
-            let name = "\((page.document?.documentURL?.lastPathComponent)!) page \(page.label!)"
+            let name = "page \(page.label!), \((page.document?.documentURL?.lastPathComponent)!)"
             let bookmark = Bookmark(name: name, page: page)
             bookmarkOutline.add(bookmark)
             bookmarkOutlineView.reloadData()
@@ -124,64 +142,7 @@ class DocumentWindowController: NSWindowController, NSWindowDelegate {
             bookmarkCount += 1
             label?.stringValue = "New Bookmark \(bookmarkCount)"
             window?.makeFirstResponder(label)
-        }
-    }
-}
-
-class LectureSetOutline: NSObject, NSOutlineViewDataSource, NSOutlineViewDelegate {
-    
-    private var documents = [PDFDocument]()
-    private var window: DocumentWindowController? = nil
-    
-   func set(window: DocumentWindowController) {
-        self.window = window
-    }
-    
-    func append(_ document: PDFDocument) {
-        documents.append(document)
-    }
-    
-    func get(byURL: URL) -> PDFDocument? {
-        return documents.first(where: {$0.documentURL == byURL})
-    }
-    
-    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-        if let pdf = item as? PDFDocument {
-            return (pdf.page(at: index))!
-        }
-        return documents[index]
-    }
-    
-    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
-        if let pdf = item as? PDFDocument {
-            return pdf.pageCount > 0
-        }
-        return false
-    }
-
-    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
-        if let pdf = item as? PDFDocument {
-            return pdf.pageCount
-        }
-        return documents.count
-    }
-    
-    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
-        let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: self) as? NSTableCellView
-        if let textField = view?.textField {
-            if let document = item as? PDFDocument {
-                textField.stringValue = (document.documentURL?.lastPathComponent)!
-            } else if let page = item as? PDFPage {
-                textField.stringValue = page.label!
-            }
-        }
-        return view
-    }
-
-    func outlineViewSelectionDidChange(_ notification: Notification) {
-        if let outlineView = notification.object as? NSOutlineView {
-            let item = outlineView.item(atRow: outlineView.selectedRow)
-            window?.lectureSelectionDidChange(item!)
+            bookmarkOutlineView.scrollRowToVisible(bookmarkOutlineView.selectedRow)
         }
     }
 }
---
+++ Viewr/OutlineModels.swift
@@ -0,0 +1,131 @@
+//
+//  OutlineModels.swift
+//  Viewr
+//
+//  Created by Rhianne Price on 30/09/17.
+//  Copyright © 2017 Rhianne Price. All rights reserved.
+//
+
+import Foundation
+
+import Quartz
+
+// A model class for the lectures currently open, used as a data source and delegate for the lecture outline in
+class LectureSetModel: NSObject, NSOutlineViewDataSource, NSOutlineViewDelegate {
+    
+    private var documents = [PDFDocument]()
+    private var window: DocumentWindowController? = nil
+    
+    func set(window: DocumentWindowController) {
+        self.window = window
+    }
+    
+    func append(_ document: PDFDocument) {
+        documents.append(document)
+    }
+    
+    func get(byURL: URL) -> PDFDocument? {
+        return documents.first(where: {$0.documentURL == byURL})
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        if let pdf = item as? PDFDocument {
+            return (pdf.page(at: index))!
+        }
+        return documents[index]
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        if let pdf = item as? PDFDocument {
+            return pdf.pageCount > 0
+        }
+        return false
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        if let pdf = item as? PDFDocument {
+            return pdf.pageCount
+        }
+        return documents.count
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: self) as? NSTableCellView
+        if let textField = view?.textField {
+            if let document = item as? PDFDocument {
+                textField.stringValue = (document.documentURL?.lastPathComponent)!
+            } else if let page = item as? PDFPage {
+                textField.stringValue = page.label!
+            }
+        }
+        return view
+    }
+    
+    func outlineViewSelectionDidChange(_ notification: Notification) {
+        if let outlineView = notification.object as? NSOutlineView {
+            let item = outlineView.item(atRow: outlineView.selectedRow)
+            window?.lectureSelectionDidChange(item!)
+        }
+    }
+}
+
+struct Bookmark {
+    let name: String
+    let page: PDFPage
+}
+
+class BookmarkOutline: NSObject, NSOutlineViewDataSource, NSOutlineViewDelegate {
+    
+    private var bookmarks = [Bookmark]()
+    private var bookmarkNames = [String]()
+    private var owner: DocumentWindowController? = nil
+    
+    
+    func add(name: String, page: PDFPage) {
+        bookmarks.append(Bookmark(name: name, page: page))
+    }
+    
+    func add(_ bookmark: Bookmark) {
+        bookmarks.append(bookmark)
+    }
+    
+    func get(byName: String) -> Bookmark? {
+        return bookmarks.first(where: {$0.name == byName})
+    }
+    
+    func set(owner: DocumentWindowController) {
+        self.owner = owner
+    }
+    
+    @IBAction func goToBookmark(_ sender: NSButtonCell) {
+        if let cell = sender.representedObject as? NSTableCellView {
+            if let bookmark = cell.objectValue as? Bookmark {
+                owner?.lectureSelectionDidChange(bookmark)
+                
+            }
+        }
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        return bookmarks[index]
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
+        return false
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+        return bookmarks.count
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "cell"), owner: self) as? NSTableCellView
+        if let textField = view?.textField {
+            if let bookmark = item as? Bookmark {
+                textField.stringValue = bookmark.name
+                view?.objectValue = bookmark
+            }
+        }
+        return view
+    }
+}
